c7bb685a382791d3b7be94cf64f31e5dbf742853
Mathies Boile Christensen <mathies88@gmail.com>
2014-05-20 23:05:52 +0200

    now loading files into memory

--- src/lightmind/project2/Project2.java
+++ src/lightmind/project2/Project2.java
@@ -3,128 +3,102 @@ package lightmind.project2;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.security.SecureRandom;
 import java.util.*;
 
 public class Project2 {
 
-    public static final Random r = new SecureRandom();
-    public static final long PRIME = powerLong(2, 31) - 1;
-    private static PrintStream printStream;
+    public static final Random RANDOM = new SecureRandom();
+//    public static final long PRIME = powerLong(2, 31) - 1;
+    public static final long PRIME = powerLong(2, 28) - 57;
+//    public static final long PRIME = powerLong(2, 20) - 3;
+//    public static final long PRIME = powerLong(2, 15) - 19;
 
+    /**
+     * The total amount of data consumes just under 6 gb of memory so set compile flag -Xmx8192m
+     */
     public static void main(String[] args) throws Exception {
-        File output = new File("output" + System.currentTimeMillis() + ".txt");
-        printStream = new PrintStream(output);
-        System.setOut(printStream);
 
-        //benchmarkDeterministic(1);
-        benchmarkRandomizedAlgorithm(Arrays.asList(7));
+//        Data data = Data.SET1;
+//        Data data = Data.SET2;
+//        Data data = Data.SET3;
+//        Data data = Data.SET4;
+//        Data data = Data.SET5;
+//        Data data = Data.SET6;
+        Data data = Data.SET7;
+
+        System.out.println("start loading data");
+        data.init();
+        System.out.println("done loading data");
+
+//        benchmarkDeterministic(data);
+        benchmarkRandomizedAlgorithm(data);
+
+        //TODO Run the randomized algorithm twice
+        //TODO Spurious true results occur when roots are found in both polynomials.
     }
 
-    private static void benchmarkRandomizedAlgorithm(List<Integer> dataSetsToRun) throws Exception {
+    private static void benchmarkRandomizedAlgorithm(Data data) throws Exception {
 
-        System.out.println("RANDOMIZED BENCHMARK:");
-        System.out.println("dataset, true, false, iterations, average running time");
-
-        long[] trueCounters = new long[7];
-        long[] falseCounters = new long[7];
-        long[] timings = new long[7];
+        int errorCounter = 0;
+        int iterations = 0;
 
         while (true) {
-            for (int i = 1; i <= 7; i++) {
-                if (dataSetsToRun.contains(i)) {
-                    for (int iteration = 0; iteration < powerLong(2, 8 - i - 1); iteration++) {
-                        long timeBefore = System.currentTimeMillis();
-                        boolean result = randomizedMultisetEquality(i);
-                        timings[i - 1] += System.currentTimeMillis() - timeBefore;
+            long timeBefore = System.currentTimeMillis();
+            boolean result = randomizedMultisetEquality(data.getLinesA(), data.getLinesB());
+            long timeAfter = System.currentTimeMillis();
+            long totalTime = timeAfter - timeBefore;
 
-                        if (result) {
-                            trueCounters[i - 1]++;
-                        } else {
-                            falseCounters[i - 1]++;
-                        }
-                    }
-
-                    System.out.println(i +
-                            ", " + trueCounters[i - 1] +
-                            ", " + falseCounters[i - 1] +
-                            ", " + (trueCounters[i - 1] + falseCounters[i - 1]) +
-                            ", " + timings[i - 1] / (trueCounters[i - 1] + falseCounters[i - 1]));
-                }
+            if (result != data.getExpectedResult()) {
+                errorCounter++;
             }
 
-            System.out.println("");
+            iterations++;
+
+            System.out.println("result: " + result + ", running time: " + totalTime + " ms, errors: " + errorCounter + ", iterations: " + iterations);
         }
     }
 
-    private static void benchmarkDeterministic(int iterations) throws Exception {
-        System.out.println("DETERMINISTIC BENCHMARK:");
-        System.out.println("dataset, result, average running time");
+    private static void benchmarkDeterministic(Data data) {
 
-        long[] timings = new long[7];
-        boolean[] results = new boolean[7];
+        while (true) {
+            long timeBefore = System.currentTimeMillis();
+            boolean result = deterministicMultisetEquality(data.getLinesA(), data.getLinesB());
+            long timeAfter = System.currentTimeMillis();
+            long totalTime = timeAfter - timeBefore;
 
-        for (int iteration = 1; iteration <= iterations; iteration++) {
-            for (int i = 1; i <= 7; i++) {
-                long timeBefore = System.currentTimeMillis();
-                results[i - 1] = deterministicMultisetEquality(i);
-                timings[i - 1] += System.currentTimeMillis() - timeBefore;
+            System.out.println("result: " + result + ", running time: " + totalTime + " ms");
+        }
+    }
+
+    public static List<String> loadFile(File f) {
+        try {
+            FileInputStream fInput = new FileInputStream(f);
+            InputStreamReader inReader = new InputStreamReader(fInput, "Cp1252");
+            List<String> elements = new ArrayList<String>(10000005);
+            Scanner sc = new Scanner(inReader);
+            String tempString;
+            int tempLength;
+
+            while (sc.hasNextLine()) {
+                tempString = sc.nextLine();
+                tempLength = tempString.length();
+                tempString = tempString.substring(1, tempLength - 1);
+                elements.add(tempString);
             }
+            sc.close();
+            return elements;
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred when loading data", e);
         }
-
-        for (int i = 1; i <= 7; i++) {
-            long timing = timings[i - 1] / iterations;
-            boolean result = results[i - 1];
-            System.out.println(i +
-                    ", " + result +
-                    ", " + timing);
-        }
-
-        System.out.println("");
     }
 
-    public static Long powerLong(long base, long exponent) {
-        long result = 1;
-        for (int i = 0; i < exponent; i++) {
-            result = result * base;
-        }
-        return result;
-    }
+    public static boolean deterministicMultisetEquality(List<String> linesA, List<String> linesB) {
+        Collections.sort(linesA);
+        Collections.sort(linesB);
 
-    public static List<String> loadFile(File f) throws Exception {
-        FileInputStream fInput = new FileInputStream(f);
-        InputStreamReader inReader = new InputStreamReader(fInput, "Cp1252");
-        List<String> elements = new ArrayList<String>(10000005);
-        Scanner sc = new Scanner(inReader);
-        String tempString;
-        int tempLength;
-
-        while (sc.hasNextLine()) {
-            tempString = sc.nextLine();
-            tempLength = tempString.length();
-            tempString = tempString.substring(1, tempLength - 1);
-            elements.add(tempString);
-        }
-        sc.close();
-        return elements;
-    }
-
-    public static boolean deterministicMultisetEquality(int i) throws Exception {
-        String setA = "ralgodata/data" + i + "a.txt";
-        String setB = "ralgodata/data" + i + "b.txt";
-
-        File fileA = new File(setA);
-        File fileB = new File(setB);
-
-        List<String> as = loadFile(fileA);
-        List<String> bs = loadFile(fileB);
-
-        Collections.sort(as);
-        Collections.sort(bs);
-
-        for (int j = 0; j < as.size(); j++) {
-            if (!as.get(j).equals(bs.get(j))) {
+        for (int j = 0; j < linesA.size(); j++) {
+            if (!linesA.get(j).equals(linesB.get(j))) {
                 return false;
             }
         }
@@ -132,41 +106,23 @@ public class Project2 {
         return true;
     }
 
-    public static boolean randomizedMultisetEquality(int i) throws Exception {
-        return randomizedMultisetEqualitySingle(i) && randomizedMultisetEqualitySingle(i);
-    }
-
-    public static boolean randomizedMultisetEqualitySingle(int i) throws Exception {
-        String setA = "ralgodata/data" + i + "a.txt";
-        String setB = "ralgodata/data" + i + "b.txt";
-
-        File fileA = new File(setA);
-        File fileB = new File(setB);
-
+    public static boolean randomizedMultisetEquality(List<String> linesA, List<String> linesB) {
         List<Long> as = generateHashFunction(80); // length of lines is at most 80
-        long zValue = Math.abs(r.nextLong()) % PRIME;
+        long zValue = Math.abs(RANDOM.nextLong()) % PRIME;
 
-        long poly1 = fingerprintFile(fileA, as, zValue);
-        long poly2 = fingerprintFile(fileB, as, zValue);
+        long poly1 = fingerprintFile(linesA, as, zValue);
+        long poly2 = fingerprintFile(linesB, as, zValue);
+
+        System.out.print("poly1: " + poly1 + ", poly2: " + poly2 + ", ");
 
         return poly1 == poly2;
     }
 
-    public static long fingerprintFile(File f, List<Long> as, long zValue) throws Exception {
-        FileInputStream fInput = new FileInputStream(f);
-        InputStreamReader inReader = new InputStreamReader(fInput, "Cp1252");
-        Scanner sc = new Scanner(inReader);
-        String tempString;
-        int tempLength;
-
+    public static long fingerprintFile(List<String> lines, List<Long> as, long zValue) {
         long result = 1;
 
-        while (sc.hasNextLine()) {
-            tempString = sc.nextLine();
-            tempLength = tempString.length();
-            tempString = tempString.substring(1, tempLength - 1); // remove [ and ]
-            long hashValue = hash(tempString, as);
-
+        for (String line : lines) {
+            long hashValue = hash(line, as);
             long value = zValue - hashValue;
 
             // we do not want negative values, javas % operator does not what we want with negative values.
@@ -174,14 +130,9 @@ public class Project2 {
                 value = PRIME + value;
             }
 
-            if (value >= PRIME) {
-                value = value % PRIME;
-            }
-
-            result = result * value;
-            result = result % PRIME;
+            result = (result * value) % PRIME;
         }
-        sc.close();
+
         return result;
     }
 
@@ -189,7 +140,7 @@ public class Project2 {
         List<Long> a = new ArrayList<Long>(size + 5);
 
         for (int i = 0; i < size; i++) {
-            a.add(Math.abs(r.nextLong()) % PRIME);
+            a.add(Math.abs(RANDOM.nextLong()) % PRIME);
         }
 
         return a;
@@ -211,5 +162,54 @@ public class Project2 {
         }
 
         return result;
+    }
+
+    public static Long powerLong(long base, long exponent) {
+        long result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result = result * base;
+        }
+
+        return result;
+    }
+
+    private enum Data {
+        SET1("ralgodata/data1a.txt", "ralgodata/data1b.txt", false),
+        SET2("ralgodata/data2a.txt", "ralgodata/data2b.txt", false),
+        SET3("ralgodata/data3a.txt", "ralgodata/data3b.txt", false),
+        SET4("ralgodata/data4a.txt", "ralgodata/data4b.txt", false),
+        SET5("ralgodata/data5a.txt", "ralgodata/data5b.txt", false),
+        SET6("ralgodata/data6a.txt", "ralgodata/data6b.txt", false),
+        SET7("ralgodata/data7a.txt", "ralgodata/data7b.txt", true);
+
+        private final String name1;
+        private final String name2;
+        private List<String> linesA;
+        private List<String> linesB;
+        private boolean expectedResult;
+
+        private Data(String name1, String name2, boolean expectedResult){
+            this.name1 = name1;
+            this.name2 = name2;
+            this.expectedResult = expectedResult;
+        }
+
+        public void init() {
+            this.linesA = loadFile(new File(name1));
+            this.linesB = loadFile(new File(name2));
+        }
+
+        public  List<String> getLinesA() {
+            return linesA;
+        }
+
+        public  List<String> getLinesB() {
+            return linesB;
+        }
+
+        public boolean getExpectedResult() {
+            return expectedResult;
+        }
     }
 }
