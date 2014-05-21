be73e12970618242b7eb00316937036049a79e6d
Mathies Boile Christensen <mathies88@gmail.com>
2014-05-21 18:29:28 +0200

    ready for testing! This time it is real

--- src/lightmind/project2/Project2.java
+++ src/lightmind/project2/Project2.java
@@ -1,109 +1,90 @@
 package lightmind.project2;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.security.SecureRandom;
 import java.text.DecimalFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Project2 {
 
-    public static final DecimalFormat format = new DecimalFormat("#.###");
+    public static final DecimalFormat FORMAT = new DecimalFormat("#.###");
     public static final Random RANDOM = new SecureRandom();
+    public static final long PRIME = powerLong(2, 20) - 185; // 1.048.391
+    //public static final long PRIME = powerLong(2, 21) - 9; // 2.097.143
+    public static final int SKIP = 50;
 
     /**
-     * The largest set consumes just under 3.5 gb of memory so set compile flag -Xmx4048m
+     * The largest set consumes just under 500 mb of memory so set compile flag -Xmx1024m
+     * Run the code twice with the different primes above.
      */
     public static void main(String[] args) throws Exception {
 
-        long prime20 = powerLong(2, 20) - 185; // 1.048.391
-        long prime21 = powerLong(2, 21) - 9; // 2.097.143
+        RandomizedBenchmarkStrategy singlePolynomialRandomizedBenchmarkStrategy = new SinglePolynomialRandomizedBenchmarkStrategy();
+        RandomizedBenchmarkStrategy doublePolynomialRandomizedBenchmarkStrategy = new DoublePolynomialRandomizedBenchmarkStrategy();
 
-        Data data = Data.SET6;
+        for (Data data : Data.values()) {
+            System.out.println("****** " + (int) data.getSize() + " lines with " + (int) data.getIterations() + " iterations ******");
 
-        //for (Data data : Data.values()) {
-            System.out.println("\n****** " + (int) data.getSize() + " lines with " + (int) data.getIterations() + " iterations ******");
-
-            System.out.println("\nrandomized single pass");
-            data.initData1Different();
-           benchmarkRandomizedAlgorithmSinglePass(data, prime20);
-         /*   benchmarkRandomizedAlgorithmSinglePass(data, prime21);
+            System.out.println("");
+            System.out.println("randomized - all lines in the two sets are different");
             data.initDataAllDifferent();
-            benchmarkRandomizedAlgorithmSinglePass(data, prime20);
-            benchmarkRandomizedAlgorithmSinglePass(data, prime21);
+            randomizedBenchmark(data, singlePolynomialRandomizedBenchmarkStrategy);
+            randomizedBenchmark(data, doublePolynomialRandomizedBenchmarkStrategy);
 
-            System.out.println("\nrandomized double pass");
+            System.out.println("");
+            System.out.println("randomized - only one line in the two sets is different");
             data.initData1Different();
-            benchmarkRandomizedAlgorithmDoublePass(data, prime20);
-            benchmarkRandomizedAlgorithmDoublePass(data, prime21);
-            data.initDataAllDifferent();
-            benchmarkRandomizedAlgorithmDoublePass(data, prime20);
-            benchmarkRandomizedAlgorithmDoublePass(data, prime21);
+            randomizedBenchmark(data, singlePolynomialRandomizedBenchmarkStrategy);
+            randomizedBenchmark(data, doublePolynomialRandomizedBenchmarkStrategy);
 
-            System.out.println("\ndeterministic single pass");
-            data.initData1Different();
-            */
-            benchmarkDeterministic(data);
+            System.out.println("");
+            System.out.println("deterministic - only one line in the two sets is different");
+            deterministicBenchmark(data);
 
-            //System.out.println("");
-        //}
-    }
-
-    private static void benchmarkRandomizedAlgorithmSinglePass(Data data, long prime) throws Exception {
-        int errorCounter = 0;
-        double totalTime = 0.0;
-
-        for (int i = 0; i < data.getIterations(); i++) {
-            double timeBefore = System.nanoTime();
-            boolean result = randomizedMultisetEqualitySinglePass(data.getLinesA(), data.getLinesB(), prime);
-            totalTime += System.nanoTime() - timeBefore;
-
-            if (result != data.getExpectedResult()) {
-                errorCounter++;
-            }
-
-            System.out.println(format.format((totalTime / (i + 1)) * Math.pow(10.0, -6.0)));
+            System.out.println("");
         }
-
-        int bitsInPrime = (int) (Math.log(prime) / Math.log(2)) + 1;
-        String averageTime = format.format((totalTime / data.getIterations()) * Math.pow(10.0, -6.0));
-        System.out.println(bitsInPrime + " bit prime, " + data.getDataType() + ", " + errorCounter + " errors, " + averageTime + " ms");
     }
 
-    private static void benchmarkRandomizedAlgorithmDoublePass(Data data, long prime) throws Exception {
+    private static void randomizedBenchmark(Data data, RandomizedBenchmarkStrategy randomizedBenchmarkStrategy) throws Exception {
         int errorCounter = 0;
-        double totalTime = 0.0;
+        long totalTime = 0;
 
         for (int i = 0; i < data.getIterations(); i++) {
-            double timeBefore = System.nanoTime();
-            boolean result = randomizedMultisetEqualityDoublePass(data.getLinesA(), data.getLinesB(), prime);
-            totalTime += System.nanoTime() - timeBefore;
+            long timeBefore = System.nanoTime();
+            boolean result = randomizedBenchmarkStrategy.randomizedMultisetEquality(data.getLinesA(), data.getLinesB());
+
+            if (i >= SKIP) {
+                totalTime += System.nanoTime() - timeBefore;
+            }
 
             if (result != data.getExpectedResult()) {
                 errorCounter++;
             }
         }
 
-        int bitsInPrime = (int) (Math.log(prime) / Math.log(2)) + 1;
-        String averageTime = format.format((totalTime / data.getIterations()) * Math.pow(10.0, -6.0));
-        System.out.println(bitsInPrime + " bit prime, " + data.getDataType() + ", " + errorCounter + " errors, " + averageTime + " ms");
+        int bitsInPrime = (int) (Math.log(PRIME) / Math.log(2)) + 1;
+        String averageTime = FORMAT.format((totalTime / (data.getIterations() - SKIP)) * Math.pow(10.0, -6.0));
+        System.out.println(bitsInPrime + " bit prime, " + errorCounter + " errors, " + averageTime + " ms, " + randomizedBenchmarkStrategy.getPolynomialType());
     }
 
-    private static void benchmarkDeterministic(Data data) {
-        double totalTime = 0.0;
+    private static void deterministicBenchmark(Data data) {
+        long totalTime = 0;
 
         for (int i = 0; i < data.getIterations(); i++) {
             Collections.shuffle(data.getLinesA());
             Collections.shuffle(data.getLinesB());
+
             long timeBefore = System.nanoTime();
             deterministicMultisetEquality(data.getLinesA(), data.getLinesB());
-            totalTime += System.nanoTime() - timeBefore;
 
-            System.out.println(format.format((totalTime / (i + 1)) * Math.pow(10.0, -6.0)));
+            if (i >= SKIP) {
+                totalTime += System.nanoTime() - timeBefore;
+            }
         }
 
-        String averageTime = format.format((totalTime / data.getIterations()) * Math.pow(10.0, -6.0));
+        String averageTime = FORMAT.format((totalTime / (data.getIterations() - SKIP)) * Math.pow(10.0, -6.0));
         System.out.println(averageTime + " ms");
     }
 
@@ -120,84 +101,17 @@ public class Project2 {
         return true;
     }
 
-    public static boolean randomizedMultisetEqualitySinglePass(List<String> linesA, List<String> linesB, long prime) {
-        List<Long> as = generateHashFunction(80, prime);
-        long zValue = Math.abs(RANDOM.nextLong()) % prime;
-
-        long poly1 = fingerprintFileSinglePass(linesA, as, zValue, prime);
-        long poly2 = fingerprintFileSinglePass(linesB, as, zValue, prime);
-
-        return poly1 == poly2;
-    }
-
-    public static boolean randomizedMultisetEqualityDoublePass(List<String> linesA, List<String> linesB, long prime) {
-        List<Long> as1 = generateHashFunction(80, prime);
-        List<Long> as2 = generateHashFunction(80, prime);
-        long zValue1 = Math.abs(RANDOM.nextLong()) % prime;
-        long zValue2 = Math.abs(RANDOM.nextLong()) % prime;
-
-        long polys1[] = fingerprintFileDoublePass(linesA, as1, as2, zValue1, zValue2, prime);
-        long polys2[] = fingerprintFileDoublePass(linesB, as1, as2, zValue1, zValue2, prime);
-
-        return polys1[0] == polys2[0] && polys1[1] == polys2[1];
-    }
-
-    public static long fingerprintFileSinglePass(List<String> lines, List<Long> as, long zValue, long prime) {
-        long result = 1;
-
-        for (String line : lines) {
-            long hashValue = hash(line, as, prime);
-            long value = zValue - hashValue;
-
-            // we do not want negative values, javas % operator does not what we want with negative values.
-            while (value < 0) {
-                value = prime + value;
-            }
-
-            result = (result * value) % prime;
-        }
-
-        return result;
-    }
-
-    public static long[] fingerprintFileDoublePass(List<String> lines, List<Long> as1, List<Long> as2, long zValue1, long zValue2, long prime) {
-        long result1 = 1;
-        long result2 = 1;
-
-        for (String line : lines) {
-            long hashValue1 = hash(line, as1, prime);
-            long hashValue2 = hash(line, as1, prime);
-            long value1 = zValue1 - hashValue1;
-            long value2 = zValue2 - hashValue2;
-
-            // we do not want negative values, javas % operator does not what we want with negative values.
-            while (value1 < 0) {
-                value1 = prime + value1;
-            }
-
-            while (value2 < 0) {
-                value2 = prime + value2;
-            }
-
-            result1 = (result1 * value1) % prime;
-            result2 = (result2 * value2) % prime;
-        }
-
-        long[] results = {result1, result2};
-        return results;
-    }
-
-    public static List<Long> generateHashFunction(int size, long prime) {
-        List<Long> a = new ArrayList<Long>(size + 5);
+    public static List<Long> generateHashFunction(int size) {
+        List<Long> a = new ArrayList<Long>(size);
 
         for (int i = 0; i < size; i++) {
-            a.add(Math.abs(RANDOM.nextLong()) % prime);
+            a.add(Math.abs(RANDOM.nextLong()) % PRIME);
         }
 
         return a;
     }
 
-    public static long hash(String x, List<Long> as, long prime) {
+    public static long hash(String x, List<Long> as) {
         char[] chars = x.toCharArray();
         long result = 0;
 
@@ -209,7 +123,7 @@ public class Project2 {
             Long aLong = as.get(i);
             char aChar = chars[i];
             result += (aChar * aLong);
-            result %= prime;
+            result %= PRIME;
         }
 
         return result;
@@ -225,14 +139,13 @@ public class Project2 {
         return result;
     }
 
-    private enum Data {
-        SET1(false, Math.pow(10.0, 1.0), Math.pow(10.0, 3.0)),
-        SET2(false, Math.pow(10.0, 2.0), Math.pow(10.0, 3.0)),
+    public static enum Data {
+        SET1(false, Math.pow(10.0, 1.0), Math.pow(10.0, 8.0)),
+        SET2(false, Math.pow(10.0, 2.0), Math.pow(10.0, 7.0)),
         SET3(false, Math.pow(10.0, 3.0), Math.pow(10.0, 6.0)),
-        SET4(false, Math.pow(10.0, 4.0), Math.pow(10.0, 3.0)),
-        SET5(false, Math.pow(10.0, 5.0), Math.pow(10.0, 1.0)),
-        SET6(false, Math.pow(10.0, 6.0), Math.pow(10.0, 1.0));
-
+        SET4(false, Math.pow(10.0, 4.0), Math.pow(10.0, 5.0)),
+        SET5(false, Math.pow(10.0, 5.0), Math.pow(10.0, 4.0)),
+        SET6(false, Math.pow(10.0, 6.0), Math.pow(10.0, 3.0));
         private List<String> linesA;
         private List<String> linesB;
         private boolean expectedResult;
@@ -312,6 +225,96 @@ public class Project2 {
 
         private String getDataType() {
             return dataType;
+        }
+    }
+
+    public static interface RandomizedBenchmarkStrategy {
+
+        public boolean randomizedMultisetEquality(List<String> linesA, List<String> linesB);
+
+        String getPolynomialType();
+    }
+
+    public static class SinglePolynomialRandomizedBenchmarkStrategy implements RandomizedBenchmarkStrategy {
+
+        @Override
+        public boolean randomizedMultisetEquality(List<String> linesA, List<String> linesB) {
+            List<Long> as = generateHashFunction(80);
+            long zValue = Math.abs(RANDOM.nextLong()) % PRIME;
+
+            long polynomial1 = fingerprint(linesA, as, zValue);
+            long polynomial2 = fingerprint(linesB, as, zValue);
+
+            return polynomial1 == polynomial2;
+        }
+
+        @Override
+        public String getPolynomialType() {
+            return "single polynomial";
+        }
+
+        public long fingerprint(List<String> lines, List<Long> as, long zValue) {
+            long result = 1;
+
+            for (String line : lines) {
+                long hashValue = hash(line, as);
+                long value = zValue - hashValue;
+
+                while (value < 0) {
+                    value = PRIME + value;
+                }
+
+                result = (result * value) % PRIME;
+            }
+
+            return result;
+        }
+    }
+
+    public static class DoublePolynomialRandomizedBenchmarkStrategy implements RandomizedBenchmarkStrategy {
+
+        @Override
+        public boolean randomizedMultisetEquality(List<String> linesA, List<String> linesB) {
+            List<Long> as1 = generateHashFunction(80);
+            List<Long> as2 = generateHashFunction(80);
+            long zValue1 = Math.abs(RANDOM.nextLong()) % PRIME;
+            long zValue2 = Math.abs(RANDOM.nextLong()) % PRIME;
+
+            long polynomials1[] = fingerprint(linesA, as1, as2, zValue1, zValue2);
+            long polynomials2[] = fingerprint(linesB, as1, as2, zValue1, zValue2);
+
+            return polynomials1[0] == polynomials2[0] && polynomials1[1] == polynomials2[1];
+        }
+
+        @Override
+        public String getPolynomialType() {
+            return "double polynomial";
+        }
+
+        public long[] fingerprint(List<String> lines, List<Long> as1, List<Long> as2, long zValue1, long zValue2) {
+            long result1 = 1;
+            long result2 = 1;
+
+            for (String line : lines) {
+                long hashValue1 = hash(line, as1);
+                long hashValue2 = hash(line, as2);
+                long value1 = zValue1 - hashValue1;
+                long value2 = zValue2 - hashValue2;
+
+                while (value1 < 0) {
+                    value1 = PRIME + value1;
+                }
+
+                while (value2 < 0) {
+                    value2 = PRIME + value2;
+                }
+
+                result1 = (result1 * value1) % PRIME;
+                result2 = (result2 * value2) % PRIME;
+            }
+
+            long[] results = {result1, result2};
+            return results;
         }
     }
 }
